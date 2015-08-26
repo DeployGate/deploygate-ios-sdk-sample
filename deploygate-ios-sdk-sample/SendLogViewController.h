560b80adf57e8ab2e5ecd6e445dc124470534074
henteko <henteko07@gmail.com>
2015-08-26 19:18:14 +0900

    add Send Cusstom Log View

---
+++ deploygate-ios-sdk-sample/SendLogViewController.h
@@ -0,0 +1,13 @@
+//
+//  SendLogViewController.h
+//  deploygate-ios-sdk-sample
+//
+//  Created by kenta.imai on 2015/08/26.
+//  Copyright (c) 2015年 kenta.imai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SendLogViewController : UIViewController
+
+@end
---
+++ deploygate-ios-sdk-sample/SendLogViewController.m
@@ -0,0 +1,51 @@
+//
+//  SendLogViewController.m
+//  deploygate-ios-sdk-sample
+//
+//  Created by kenta.imai on 2015/08/26.
+//  Copyright (c) 2015年 kenta.imai. All rights reserved.
+//
+
+#import "SendLogViewController.h"
+#import <DeployGateSDK/DeployGateSDK.h>
+
+@interface SendLogViewController ()
+@property (strong, nonatomic) IBOutlet UITextView *textView;
+
+@end
+
+@implementation SendLogViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)sendLog:(UIBarButtonItem *)sender {
+    NSString *text = self.textView.text;
+    self.textView.text = @"";
+    DGSLog(@"%@", text);
+
+    [[[UIAlertView alloc] initWithTitle:@"DGSLog Done"
+                                message:@"Require relaunch app"
+                               delegate:self
+                      cancelButtonTitle:@"Continue"
+                      otherButtonTitles:@"Relaunch", nil] show];
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == 1) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundeclared-selector"
+        [[UIApplication sharedApplication] performSelector:@selector(terminateWithSuccess)];
+#pragma clang diagnostic pops
+    }
+}
+
+@end
