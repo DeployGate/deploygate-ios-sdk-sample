dd87fd80acf70fa52602b52eae9e4b8f7c004c05
henteko <henteko07@gmail.com>
2015-08-31 16:40:17 +0900

    init swift

---
+++ swift/deploygate-ios-sdk-swift-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  deploygate-ios-sdk-swift-sample
+//
+//  Created by kenta.imai on 2015/08/31.
+//  Copyright (c) 2015年 DeployGate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
