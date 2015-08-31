d398afb33b60af8e93450789e8065687e5c838fe
henteko <henteko07@gmail.com>
2015-08-31 18:13:14 +0900

    add Send Custom Log

---
+++ swift/deploygate-ios-sdk-swift-sample/SendLogViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SendLogViewController.swift
+//  deploygate-ios-sdk-swift-sample
+//
+//  Created by kenta.imai on 2015/08/31.
+//  Copyright (c) 2015年 DeployGate. All rights reserved.
+//
+
+import UIKit
+
+class SendLogViewController: UIViewController {
+    @IBOutlet var textView: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    @IBAction func sendLog(sender: AnyObject) {
+        var text = textView.text
+        textView.text = ""
+
+        DGSLogv("%@", getVaList([text]))
+        var alert:UIAlertController = UIAlertController(title: "DGSLog Done", message: "Require relaunch app", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Continue", style: UIAlertActionStyle.Cancel, handler:nil))
+        alert.addAction(UIAlertAction(title: "Relaunch",
+            style: UIAlertActionStyle.Default,
+            handler:{
+                (action:UIAlertAction!) -> Void in
+                exit(0) // finish application
+        }))
+        presentViewController(alert, animated: true, completion: nil)
+    }
+}
